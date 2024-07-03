a5b863c7342d14f416451d85180d7580ffe3d1a2
Jericho Wenzel Serrano <jerichowenzel@JerichoMB.local>
2024-07-04 02:22:23 +0800

    change delay for lists of works or projects animation

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ document.addEventListener('DOMContentLoaded', ()=> {
     tl.from(".heading-container", {y: 50, opacity: 0})
         .to("p", {clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"}, "-=.7")
         .to("h1", {clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"}, "-=.7")
-        .from(".row .col-6.col-lg-3.flex-grow-1", {y: 50, opacity: 0, stagger: .3}, "-=.7")
+        .from(".row .col-6.col-lg-3.flex-grow-1", {y: 50, opacity: 0, stagger: .3}, "-=.3")
 })
